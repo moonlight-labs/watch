932881517d3315a6673ba57e4f6a57271a6b8dad
Cosimo Barbato <cbarb001@gmail.com>
2015-03-31 14:40:59 -0700

    Add JsonBloomfilter gem Validate bloom filter performance over naive SQL

--- test/watch_test.rb
+++ test/watch_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+require 'json-bloomfilter'
+
 class WatchTest < ActiveSupport::TestCase
   def setup
     Watch::Watch.destroy_all
@@ -79,18 +81,68 @@ class WatchTest < ActiveSupport::TestCase
     assert { f.watchers.collect(&:watcher).include?(u) }
   end
 
-  test 'Performance is optimized for getting large number of followers' do
+  # test 'Performance is optimized for getting large number of followers' do
     
-    (1..(1000-Firm.count)).each {|i| Firm.create(name: ::Faker::Company.name) }
+  #   (1..(1000-Firm.count)).each {|i| Firm.create(name: ::Faker::Company.name) }
+
+  #   u = User.first
+  #   Firm.find_each{|f| u.watch(f) }
+
+  #   time = Benchmark.realtime do
+  #     watched = u.watches.all.collect(&:watchable)
+  #     puts "#{watched.count}\t#{watched.last}"
+  #   end
+  #   puts time
+  # end
+
+  test 'Simple bloomfilter' do
+    filter = JsonBloomfilter.build 10000, 0.01
+  end
+  
+  test 'Check watch status of random list of objects quickly' do
+    users_count = 10000
+    firms_count = 10000
+    check_count = 100
+
+    User.transaction do
+      (1..(firms_count-Firm.count)).each {|i| Firm.create(name: ::Faker::Company.name) }
+      (1..(users_count-User.count)).each {|i| Firm.create(name: ::Faker::Name.name) }
+    end
 
     u = User.first
-    Firm.find_each{|f| u.watch(f) }
+
+    firms = Firm.order('random()').limit(check_count*10)
+    firms.each { |f| u.watch(f) }
+
+    firms = Firm.order('random()').limit(check_count)
+    
+    # make sure the firm objects are actually loaded
+    t = firms.collect(&:to_s)
 
     time = Benchmark.realtime do
-      watched = u.watches.all.collect(&:watchable)
-      puts "#{watched.count}\t#{watched.last}"
+      firms.collect do |f| 
+        u.watches?(f) 
+      end 
     end
-    puts time
+    puts "#{time}"
+
+
+    filter = JsonBloomfilter.build 10000, 0.01
+    filter.add u.watches.pluck(:watchable_type,:watchable_id)
+    
+    true_val = u.watches.limit(1).pluck(:watchable_type, :watchable_id)
+    false_val = ["Firm", 12000]
+
+    assert { filter.test true_val }
+    assert { filter.test(false_val)  == false}
+
+    time = Benchmark.realtime do
+      firms.collect do |f| 
+        filter.test(["Firm", f.id]) 
+      end 
+    end
+    puts "#{time}"
+
   end
 
 end
--- watch.gemspec
+++ watch.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency "rails", "~> 4.2.1"
+  s.add_dependency 'json-bloomfilter'
 
   s.add_development_dependency "pg"
 
