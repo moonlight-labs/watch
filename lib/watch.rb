1bc457cbdc7daa83f5cb0418bb3d9241625547e9
Kevin Sylvestre <kevin@ksylvest.com>
2015-06-09 10:45:29 -0700

    moving to concerns to avoid duplicate declarations (concerns do the same thing in newer Rails)

--- lib/watch.rb
+++ lib/watch.rb
@@ -1,19 +1,4 @@
 require "watch/engine"
 
-# Augmentations
-class ::Object
-  def self.augment(*mods)
-    include *mods
-    mods.each {|mod| class_eval &mod.augmentation }
-  end
-end
-
-class ::Module
-  def augmentation(&block)
-    @augmentation ||= block
-  end
-end
-
-
 module Watch
 end
