8caa6e67af72c87c6dd9ec0ea48eca1e91cbb389
Kevin Sylvestre <kevin@ksylvest.com>
2015-06-09 17:12:13 -0700

    fixing namespace lookup bug

--- app/models/watch/watcher.rb
+++ app/models/watch/watcher.rb
@@ -6,12 +6,12 @@ module Watch
       has_many :watches, as: :watcher, dependent: :destroy, class_name: "Watch::Watch"
     end
 
-    def watch(object)
-      Watch::Watch.watch(self, object)
+    def watch!(object)
+      ::Watch::Watch.watch!(self, object)
     end
 
-    def unwatch(object)
-      Watch::Watch.unwatch(self, object)
+    def unwatch!(object)
+      ::Watch::Watch.unwatch!(self, object)
     end
 
     def watching?(object)
