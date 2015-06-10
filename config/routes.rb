bfeac8e224b842a21ac21e44ee4aea9f68986d7a
Kevin Sylvestre <kevin@ksylvest.com>
2015-06-09 17:12:03 -0700

    strange but these aren't exactly RESTFUL endpoints (unwatching doesn't take in an ID - just the components)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Watch::Engine.routes.draw do
-  resources :watches, only: [:index, :create, :destroy]
+  resources :watches, only: [:index]
+  post 'watches', to: 'watches#create'
+  delete 'watches', to: 'watches#destroy'
 end
