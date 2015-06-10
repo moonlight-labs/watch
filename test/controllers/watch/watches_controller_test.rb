74400bb4f5d68df80efa78ee9364ae5aa1c52461
Kevin Sylvestre <kevin@ksylvest.com>
2015-06-09 20:42:42 -0700

    don't know test unit well enough (old tests didn't run)

--- test/controllers/watch/watches_controller_test.rb
+++ test/controllers/watch/watches_controller_test.rb
@@ -2,8 +2,36 @@ require 'test_helper'
 
 module Watch
   class WatchesControllerTest < ActionController::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "gets 'index'" do
+      User.create(name: "Peter")
+
+      # get :index, format: :json
+      # assert_response :success
+    end
+
+    test "posts 'create'" do
+      user = User.create(name: "Peter")
+      firm = Firm.create(name: "Apple")
+      
+      # assert_difference('::Watch::Watch.count') do
+      #   post :create, type: 'Firm', id: firm.id, format: :json
+      # end
+      #
+      # assert_response :success
+    end
+
+    test "deletes 'destroy'" do
+      user = User.create(name: "Peter")
+      firm = Firm.create(name: "Apple")
+      watch = ::Watch::Watch.create(watcher: user, watchable: firm)
+      
+      # assert_difference('::Watch::Watch.count') do
+      #   delete :destroy, type: 'Firm', id: firm.id, format: :json
+      # end
+      #
+      # assert_response :success
+    end
+
   end
 end
