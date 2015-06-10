f620215a9474a9dff0b894e5f721036c9a417fa7
Kevin Sylvestre <kevin@ksylvest.com>
2015-06-09 17:12:25 -0700

    added in a proper helper that uses the object ID and class

--- app/helpers/watch/application_helper.rb
+++ app/helpers/watch/application_helper.rb
@@ -2,7 +2,7 @@ module Watch
   module ApplicationHelper
 
     def react_watch_widget(object, options = {})
-      react_component 'Components.Watch.Widget'
+      react_component 'Components.Watch.Widget', { id: object.id, type: "#{object.class}" }
     end
 
   end
